5dfad69e10a91bc5130c95bd0dd4c987e0e68dc0
RohitMangdare <108042722+RohitMangdare@users.noreply.github.com>
2022-06-23 08:57:54 +0530

    basic python programs

---
+++ basic programs/02.py
@@ -0,0 +1,6 @@
+r=float(input("enter radius of circle"))
+print("radius of circle=",r)
+area=3.14*r*r
+print("area of circle=",area)
+circum=2*3.14*r
+print("circumference of circle=",circum)
---
+++ basic programs/03.py
@@ -0,0 +1,8 @@
+l=float(input("enter length of rectangle"))
+b=float(input("enter bridth of rectangle"))
+
+area=l*b
+print("area of rectangle=",area)
+
+peri=2*(l*b)
+print("perimiter of rectengle=",peri)
